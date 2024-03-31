830021c7024c2ecb94e666a067feef85e6b10180
sakim <sck10001@gmail.com>
2024-03-31 14:29:22 -0700

    MVP

--- Teamlog/init.lua
+++ Teamlog/init.lua
@@ -1,16 +1,123 @@
--- constants from chatlog. Prob will need? TODO: Delete as needed. 
+local core_mainmenu = require("core_mainmenu")
+local cfg = require("Teamlog.configuration")
+local optionsLoaded, options = pcall(require, "Teamlog.options")
+
+local optionsFileName = "addons/Teamlog/options.lua"
+local firstPresent = true
+local ConfigurationWindow
+
+-- constants --
 local LOCALES = "EJTKB"
 local MSG_REPLACE = "\9([" .. LOCALES .. "])"
-local MAX_GAME_LOG = 30 -- need to test - supposedly it's 30? TODO
 local MAX_MSG_SIZE = 100
-local counter = 0 -- use as unique identifier
-local chat_memory = {} -- use as a table 
+local chat_memory = {} -- table, stores read msgs from mem
 local ordered_messages = {} -- messages in order
 
 -- initialize an empty table for the chat memory
 for i = 0, 58, 2 do -- 30 messages, 0 indexed
     chat_memory[i/2+1] = ""
 end
+
+-- Helpers in solylib
+local function NotNilOrDefault(value, default)
+    if value == nil then
+        return default
+    else
+        return value
+    end
+end
+local function GetPosBySizeAndAnchor(_x, _y, _w, _h, _anchor)
+    local x
+    local y
+
+    local resW = pso.read_u16(0x00A46C48)
+    local resH = pso.read_u16(0x00A46C4A)
+
+    -- Top left
+    if _anchor == 1 then
+        x = _x
+        y = _y
+
+    -- Left
+    elseif _anchor == 2 then
+        x = _x
+        y = (resH / 2) - (_h / 2) + _y
+
+    -- Bottom left
+    elseif _anchor == 3 then
+        x = _x
+        y = resH - _h + _y
+
+    -- Top
+    elseif _anchor == 4 then
+        x = (resW / 2) - (_w / 2) + _x
+        y = _y
+
+    -- Center
+    elseif _anchor == 5 then
+        x = (resW / 2) - (_w / 2) + _x
+        y = (resH / 2) - (_h / 2) + _y
+
+    -- Bottom
+    elseif _anchor == 6 then
+        x = (resW / 2) - (_w / 2) + _x
+        y = resH - _h + _y
+
+    -- Top right
+    elseif _anchor == 7 then
+        x = resW - _w + _x
+        y = _y
+
+    -- Right
+    elseif _anchor == 8 then
+        x = resW - _w + _x
+        y = (resH / 2) - (_h / 2) + _y
+
+    -- Bottom right
+    elseif _anchor == 9 then
+        x = resW - _w + _x
+        y = resH - _h + _y
+
+    -- Whatever
+    else
+        x = _x
+        y = _y
+    end
+
+    return { x, y }
+end
+-- End of helpers in solylib
+
+
+local function SaveOptions(options)
+    local file = io.open(optionsFileName, "w")
+    if file ~= nil then
+        io.output(file)
+
+        io.write("return\n")
+        io.write("{\n")
+        io.write(string.format("    configurationEnableWindow = %s,\n", tostring(options.configurationEnableWindow)))
+        io.write(string.format("    enable = %s,\n", tostring(options.enable)))
+        io.write(string.format("    useCustomTheme = %s,\n", tostring(options.useCustomTheme)))
+        io.write(string.format("    fontScale = %s,\n", tostring(options.fontScale)))
+        io.write("\n")
+        io.write(string.format("    clEnableWindow = %s,\n", tostring(options.clEnableWindow)))
+        io.write(string.format("    clChanged = %s,\n", tostring(options.clChanged)))
+        io.write(string.format("    clAnchor = %i,\n", options.clAnchor))
+        io.write(string.format("    clX = %i,\n", options.clX))
+        io.write(string.format("    clY = %i,\n", options.clY))
+        io.write(string.format("    clW = %i,\n", options.clW))
+        io.write(string.format("    clH = %i,\n", options.clH))
+        io.write(string.format("    clNoTitleBar = \"%s\",\n", options.clNoTitleBar))
+        io.write(string.format("    clNoResize = \"%s\",\n", options.clNoResize))
+        io.write(string.format("    clNoMove = \"%s\",\n", options.clNoMove))
+        io.write(string.format("    clTransparentWindow = %s,\n", tostring(options.clTransparentWindow)))
+        io.write("}\n")
+
+        io.close(file)
+    end
+end
+
 
 local function get_team_log()
     --[[
@@ -41,7 +148,6 @@ local function get_team_log()
         exactly once a frame? 
     ]]
 
-
     -- constantly check the memory addresses for new messages 
     for i = 0, 58, 2 do -- 30 messages, 0 indexed
         --[[
@@ -63,12 +169,30 @@ local function get_team_log()
         chat_memory[i/2+1] = message
     end
 
-    --print the ordered messages
-    for index, value in ipairs(ordered_messages) do
-        imgui.Text(index)
-        imgui.Text(value)
+    -- WARNING: trying to render an empty table will crash the game
+    if #ordered_messages == 0 then
+        -- Do nothing 
+    else
+        -- only if there are messages we attempt to render
+        last_ten_messages = get_last_ten_elements(ordered_messages)
+        for index, value in ipairs(last_ten_messages) do
+            imgui.Text(value)
+        end
+    end
+end
+
+function get_last_ten_elements(input_table)
+    -- get the last 10 elements of a table
+    local length = #input_table
+    local result = {}
+    local start_index = math.max(length - 9, 1)
+    
+    for i = start_index, length do
+        table.insert(result, input_table[i])
     end
     
+    return result
+
 end
 
 
@@ -85,10 +209,60 @@ function add_timestamp(message)
 end 
 
 local function present()
-    get_team_log()
+    -- If the addon has never been used, open the config window
+    -- and disable the config window setting
+    if options.configurationEnableWindow then
+        ConfigurationWindow.open = true
+        options.configurationEnableWindow = false
+    end
+    
+    ConfigurationWindow.Update()
+    if ConfigurationWindow.changed then
+        ConfigurationWindow.changed = false
+        SaveOptions(options)
+    end
+
+    -- Global enable here to let the configuration window work
+    if options.enable == false then
+        return
+    end
+
+    if firstPresent or options.clChanged then
+        options.clChanged = false
+        local ps = GetPosBySizeAndAnchor(options.clX, options.clY, options.clW, options.clH, options.clAnchor)
+        imgui.SetNextWindowPos(ps[1], ps[2], "Always");
+        imgui.SetNextWindowSize(options.clW, options.clH, "Always");
+    end
+
+    if options.clTransparentWindow == true then
+        imgui.PushStyleColor("WindowBg", 0.0, 0.0, 0.0, 0.0)
+    end
+
+    if imgui.Begin("Teamlog", nil, { options.clNoTitleBar, options.clNoResize, options.clNoMove }) then
+        imgui.SetWindowFontScale(options.fontScale)
+        get_team_log()
+    end
+    imgui.End()
+
+    if options.clTransparentWindow == true then
+        imgui.PopStyleColor()
+    end
+
+    if firstPresent then
+        firstPresent = false
+    end
+    --get_team_log()
 end
 
 local function init()
+    ConfigurationWindow = cfg.ConfigurationWindow(options)
+
+    local function mainMenuButtonHandler()
+        ConfigurationWindow.open = not ConfigurationWindow.open
+    end
+
+    core_mainmenu.add_button("Teamlog", mainMenuButtonHandler)
+
     return
     {
         name = "TeamLog",
---
+++ Teamlog/options.lua
@@ -0,0 +1,19 @@
+return
+{
+    configurationEnableWindow = false,
+    enable = true,
+    useCustomTheme = false,
+    fontScale = 1,
+
+    clEnableWindow = true,
+    clChanged = true,
+    clAnchor = 6,
+    clX = 600,
+    clY = -200,
+    clW = 450,
+    clH = 350,
+    clNoTitleBar = "",
+    clNoResize = "",
+    clNoMove = "",
+    clTransparentWindow = true,
+}
